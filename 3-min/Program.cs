7f6fc70693517515393fb357a2aaa5a3fb3a8c3e
k1dobu3 <91422323+k1dobu3@users.noreply.github.com>
2021-10-19 15:10:35 +0300

    Add files via upload

---
+++ 3-min/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m;
+            Console.WriteLine("Введите три числа, чтобы определить меньшее из них:");
+
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            int z = Convert.ToInt32(Console.ReadLine());
+
+            m = x < y ? x < z ? x : z : y < z ? y : z;
+            Console.WriteLine(m);
+
+
+
+        }
+    }
+}
